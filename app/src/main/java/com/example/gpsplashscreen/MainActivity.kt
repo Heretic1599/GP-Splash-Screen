1264265192ffa5cbb348084c6638aea89d9aee06
Heretic1599 <tylsal2949@students.ecpi.edu>
2026-06-04 11:13:42 +0000

    Initial Android project structure

---
+++ app/src/main/java/com/example/gpsplashscreen/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.gpsplashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+// Tyler Salvador
+// Guided Practice Splash Screen
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity2::class.java))
+            finish()
+        }, 3000)
+    }
+}
---
+++ app/src/main/java/com/example/gpsplashscreen/MainActivity2.kt
@@ -0,0 +1,15 @@
+package com.example.gpsplashscreen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// Tyler Salvador
+// Guided Practice Splash Screen
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
